363ead5e3140333b65dc31d304962b21679c04fb
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-08-19 20:55:39 +0800

    refactor(module): remove unused PrismaService import from TasksModule

--- apps/api/src/tasks/tasks.module.ts
+++ apps/api/src/tasks/tasks.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
-import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  imports: [PrismaService],
   controllers: [TasksController],
   providers: [TasksService],
 })
