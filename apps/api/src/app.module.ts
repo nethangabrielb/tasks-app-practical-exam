06eb58bfae8dc4c6f9b5dee40df4c2b1c143c9e2
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-08-19 19:09:42 +0800

    feat(api): initialize tasks module with CRUD template

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { PrismaModule } from './prisma/prisma.module.js';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
