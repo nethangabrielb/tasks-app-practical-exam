0579b1dba1e5553453f9b0c759601fce64b0736d
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-08-19 22:15:53 +0800

    feat(validators): extend createTaskSchema to include task ID

--- packages/validators/create-task.dto.ts
+++ packages/validators/create-task.dto.ts
@@ -8,5 +8,10 @@ export const createTaskSchema = z.object({
   status: TaskStatus.default("INCOMPLETE"),
 });
 
+const taskSchema = createTaskSchema.extend({
+  id: z.number(),
+});
+
 export type CreateTaskDto = z.infer<typeof createTaskSchema>;
 export type TaskStatus = z.infer<typeof TaskStatus>;
+export type Task = z.infer<typeof taskSchema>;
